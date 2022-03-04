ce3317cae64c4bea7f1b7749c6a6f5c6e14472d1
Mike Darche <mdarche@users.noreply.github.com>
2022-03-04 13:48:15 -0500

    initial commit

---
+++ includes/class-next-revalidate-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://twitter.com/mikedarche
+ * @since      1.0.0
+ *
+ * @package    Next_Revalidate
+ * @subpackage Next_Revalidate/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Next_Revalidate
+ * @subpackage Next_Revalidate/includes
+ * @author     Mike Darche <mike@oneofnone.io>
+ */
+class Next_Revalidate_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
